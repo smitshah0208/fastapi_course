d90d59d8f10df5d98d9350def0dd58e01d6ee2c1
smitshah0208 <smitshah0208@gmail.com>
2024-12-22 16:26:47 +0530

    till get operation

--- main.py
+++ main.py
@@ -1,15 +1,40 @@
 from fastapi import FastAPI
+from typing import Optional
 
 
 # Create an instance of the class FastApi
 app = FastAPI()
 
-@app.get('/')
-def index():
+@app.get('/blog')
+def index(limit:int= 10,published:bool=True,sort: Optional[str]=None):
+    #Only get 10 published blogs
+    if published:
+        return {
+            "data": f"{limit} published blogs from the db"
+        }
+    else:
+        return{
+            "data": f"{limit} all blogs from the db"
+        }
+
+
+@app.get("/blog/unpublished")
+def unpublished():
     return {
-        "data": {"name": "SMIT","age":26}
+        "data": "all unpublished blogs."
     }
 
-@app.get("/about")
-def about():
-    return {"data": "about page"}
+
+@app.get("/blog/{id}")
+def show(id:int):
+    # fetch blog with id = id 
+    return {"data": id}
+
+
+@app.get("/blog/{id}/comments")
+def comments(id:int):
+    #fecth comments of blog with id = id
+    return {
+        "data": {id: f"This is the comment number {id}."}
+        }
+
